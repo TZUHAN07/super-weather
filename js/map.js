65fea3ce6f840c6a74abcee9eb27aad3dc94f0c3
PeiYing <peggy2000000@gmail.com>
2023-01-04 21:53:33 +0800

    add hover effect of map

---
+++ js/map.js
@@ -0,0 +1,185 @@
+const keelung = document.querySelector('text[x="500"][y="85"]')
+const taipei = document.querySelector('text[x="480"][y="70"]')
+const newTaipei = document.querySelector('text[x="455"][y="140"]')
+const yilan = document.querySelector('text[x="510,500,490"][y="165,190,215"]')
+const hualien = document.querySelector('text[x="465,455,445"][y="300,350,400"]')
+const taitung = document.querySelector('text[x="385,375,350"][y="530,580,630"]')
+const taoyuan = document.querySelector('text[x="395"][y="130, 150,170"]')
+const hsinchuCity = document.querySelector('text[x="340"][y="160"]')
+const hsinchuCountry = document.querySelector('text[x="380"][y="173,193,213"]')
+const miaoli = document.querySelector('text[x="320"][y="225,235,245"][dx="0,5,5"]')
+const taichung = document.querySelector('text[x="290"][y="295"]')
+const nantou = document.querySelector('text[x="325,350,375"][y="375"]')
+const changhua = document.querySelector('text[x="237"][y="350"]')
+const yunlin = document.querySelector('text[x="220"][y="405"]')
+const chiayiCity = document.querySelector('text[x="215,255,280"][y="460,438,460"]')
+const chiayiCountry = document.querySelector('text[x="220"][y="405"]')
+const tainan = document.querySelector('text[x="195,220,245"][y="530"]')
+const kaohsiung = document.querySelector('text[x="320,290,260"][y="510,550,585"]')
+const pingtung = document.querySelector('text[x="290,290,320"][y="605,665,725"]')
+const lienchiang = document.querySelector('text[x="90"][y="120"][dx="0,5,5"]')
+const kinmen = document.querySelector('text[x="60"][y="280"][dx="0,5,5"]')
+const penghu = document.querySelector('text[ x="50"][y="375"][dx="0,5,5"]')
+
+document.querySelector('[data-name="keelung_city"]').addEventListener('mouseover', () => {
+    keelung.style.display = 'block';
+});
+document.querySelector('[data-name="keelung_city"]').addEventListener('mouseout', () => {
+    keelung.style.display = 'none';
+});
+document.querySelector('[data-name="taipei_city"]').addEventListener('mouseover', () => {
+    taipei.style.display = 'block';
+});
+document.querySelector('[data-name="taipei_city"]').addEventListener('mouseout', () => {
+    taipei.style.display = 'none';
+});
+document.querySelector('[data-name="new_taipei_city"]').addEventListener('mouseover', () => {
+    newTaipei.style.display = 'block';
+});
+document.querySelector('[data-name="new_taipei_city"]').addEventListener('mouseout', () => {
+    newTaipei.style.display = 'none';
+});
+document.querySelector('[data-name="path2523"]').addEventListener('mouseover', () => {
+    yilan.style.display = 'block';
+});
+document.querySelector('[data-name="path2523"]').addEventListener('mouseout', () => {
+    yilan.style.display = 'none';
+});
+document.querySelector('[data-name="yilan_country"]').addEventListener('mouseover', () => {
+    yilan.style.display = 'block';
+});
+document.querySelector('[data-name="yilan_country"]').addEventListener('mouseout', () => {
+    yilan.style.display = 'none';
+});
+document.querySelector('[data-name="hualien_country"]').addEventListener('mouseover', () => {
+    hualien.style.display = 'block';
+});
+document.querySelector('[data-name="hualien_country"]').addEventListener('mouseout', () => {
+    hualien.style.display = 'none';
+});
+document.querySelector('[data-name="path2525"]').addEventListener('mouseover', () => {
+    taitung.style.display = 'block';
+});
+document.querySelector('[data-name="path2525"]').addEventListener('mouseout', () => {
+    taitung.style.display = 'none';
+});
+document.querySelector('[data-name="path2527"]').addEventListener('mouseover', () => {
+    taitung.style.display = 'block';
+});
+document.querySelector('[data-name="path2527"]').addEventListener('mouseout', () => {
+    taitung.style.display = 'none';
+});
+document.querySelector('[data-name="taitung_country"]').addEventListener('mouseover', () => {
+    taitung.style.display = 'block';
+});
+document.querySelector('[data-name="taitung_country"]').addEventListener('mouseout', () => {
+    taitung.style.display = 'none';
+});
+document.querySelector('[data-name="taoyuan_country"]').addEventListener('mouseover', () => {
+    taoyuan.style.display = 'block';
+});
+document.querySelector('[data-name="taoyuan_country"]').addEventListener('mouseout', () => {
+    taoyuan.style.display = 'none';
+});
+document.querySelector('[data-name="hsinchu_city"]').addEventListener('mouseover', () => {
+    hsinchuCity.style.display = 'block';
+});
+document.querySelector('[data-name="hsinchu_city"]').addEventListener('mouseout', () => {
+    hsinchuCity.style.display = 'none';
+});
+document.querySelector('[data-name="hsinchu_country"]').addEventListener('mouseover', () => {
+    hsinchuCountry.style.display = 'block';
+});
+document.querySelector('[data-name="hsinchu_country"]').addEventListener('mouseout', () => {
+    hsinchuCountry.style.display = 'none';
+});
+document.querySelector('[data-name="miaoli_country"]').addEventListener('mouseover', () => {
+    miaoli.style.display = 'block';
+});
+document.querySelector('[data-name="miaoli_country"]').addEventListener('mouseout', () => {
+    miaoli.style.display = 'none';
+});
+document.querySelector('[data-name="taichung_city"]').addEventListener('mouseover', () => {
+    taichung.style.display = 'block';
+});
+document.querySelector('[data-name="taichung_city"]').addEventListener('mouseout', () => {
+    taichung.style.display = 'none';
+});
+document.querySelector('[data-name="nantou_country"]').addEventListener('mouseover', () => {
+    nantou.style.display = 'block';
+});
+document.querySelector('[data-name="nantou_country"]').addEventListener('mouseout', () => {
+    nantou.style.display = 'none';
+});
+document.querySelector('[data-name="changhua_country"]').addEventListener('mouseover', () => {
+    changhua.style.display = 'block';
+});
+document.querySelector('[data-name="changhua_country"]').addEventListener('mouseout', () => {
+    changhua.style.display = 'none';
+});
+document.querySelector('[data-name="yunlin_country"]').addEventListener('mouseover', () => {
+    yunlin.style.display = 'block';
+});
+document.querySelector('[data-name="yunlin_country"]').addEventListener('mouseout', () => {
+    yunlin.style.display = 'none';
+});
+document.querySelector('[data-name="chiayi_city"]').addEventListener('mouseover', () => {
+    chiayiCity.style.display = 'block';
+});
+document.querySelector('[data-name="chiayi_city"]').addEventListener('mouseout', () => {
+    chiayiCity.style.display = 'none';
+});
+document.querySelector('[data-name="chiayi_country"]').addEventListener('mouseover', () => {
+    chiayiCountry.style.display = 'block';
+});
+document.querySelector('[data-name="chiayi_country"]').addEventListener('mouseout', () => {
+    chiayiCountry.style.display = 'none';
+});
+document.querySelector('[data-name="tainan_city"]').addEventListener('mouseover', () => {
+    tainan.style.display = 'block';
+});
+document.querySelector('[data-name="tainan_city"]').addEventListener('mouseout', () => {
+    tainan.style.display = 'none';
+});
+document.querySelector('[data-name="kaohsiung_city"]').addEventListener('mouseover', () => {
+    kaohsiung.style.display = 'block';
+});
+document.querySelector('[data-name="kaohsiung_city"]').addEventListener('mouseout', () => {
+    kaohsiung.style.display = 'none';
+});
+document.querySelector('[data-name="path2462"]').addEventListener('mouseover', () => {
+    pingtung.style.display = 'block';
+});
+document.querySelector('[data-name="path2462"]').addEventListener('mouseout', () => {
+    pingtung.style.display = 'none';
+});
+document.querySelector('[data-name="pingtung_country"]').addEventListener('mouseover', () => {
+    pingtung.style.display = 'block';
+});
+document.querySelector('[data-name="pingtung_country"]').addEventListener('mouseout', () => {
+    pingtung.style.display = 'none';
+});
+document.querySelector('[data-name="g4313"]').addEventListener('mouseover', () => {
+    lienchiang.style.display = 'block';
+});
+document.querySelector('[data-name="g4313"]').addEventListener('mouseout', () => {
+    lienchiang.style.display = 'none';
+});
+document.querySelector('[data-name="g4342"]').addEventListener('mouseover', () => {
+    kinmen.style.display = 'block';
+});
+document.querySelector('[data-name="g4342"]').addEventListener('mouseout', () => {
+    kinmen.style.display = 'none';
+});
+document.querySelector('[data-name="kinmen_country"]').addEventListener('mouseover', () => {
+    kinmen.style.display = 'block';
+});
+document.querySelector('[data-name="kinmen_country"]').addEventListener('mouseout', () => {
+    kinmen.style.display = 'none';
+});
+document.querySelector('[data-name="penghu_country"]').addEventListener('mouseover', () => {
+    penghu.style.display = 'block';
+});
+document.querySelector('[data-name="penghu_country"]').addEventListener('mouseout', () => {
+    penghu.style.display = 'none';
+});
